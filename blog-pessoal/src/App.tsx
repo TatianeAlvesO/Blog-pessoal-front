186bfffa899b66c1d4b3862d1c30431ae47a9061
tatianealves <tatianealvesdeoliveira@outlook.com.br>
2022-11-01 18:35:47 -0300

    Sistemas de rotas do BP

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 import Home from './paginas/home/Home';
 import './App.css';
+import Login from "./paginas/login/Login";
 
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-        <Home />
+      <div style={{minHeight: "100vh"}}>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} /> 
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
