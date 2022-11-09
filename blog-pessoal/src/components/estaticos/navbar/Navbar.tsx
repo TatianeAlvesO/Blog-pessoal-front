792e2a2b2241d31a8b6a04bffbe356b7c7659bb2
tatianealves <tatianealvesdeoliveira@outlook.com.br>
2022-11-09 16:59:04 -0300

    Adição das camadas Tema e Postagem

--- blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -1,54 +1,72 @@
 import React from "react";
-import {AppBar, Toolbar, Typography} from "@material-ui/core";
-import {Box} from "@mui/material";
-import {Link} from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 import "./Navbar.css"
 
 function Navbar() {
+    const [token, setToken] = useLocalStorage("token");
+    let navigate = useNavigate();
+
+    function goLogout() {
+        setToken("")
+        alert("Usuário deslogado")
+        navigate("/login")
+    }
+
     return (
         <>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <Box className="cursor">
+                    <Box className='cursor'>
                         <Typography variant="h5" color="inherit">
                             BlogPessoal
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                home
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
-                            </Typography>
-                        </Box>
-                        <Link to="/login" className="text-decorator-none">
-                        <Box mx={1} className="cursor">
+                        <Link to="/home" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    home
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/temas" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    temas
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/formularioTema" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    cadastrar tema
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Box mx={1} className='cursor' onClick={goLogout}>
                             <Typography variant="h6" color="inherit">
                                 logout
                             </Typography>
                         </Box>
-                        </Link>
-                        
+
                     </Box>
+
                 </Toolbar>
             </AppBar>
         </>
-    );
+    )
 }
 
 export default Navbar;
